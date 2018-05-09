4eb041f0576f2b7eb9395e3eb3712b04ed3955dd
Carlos Villela <cv@lixo.org>
2018-05-09 12:01:30 -0300

    Brew formula update for sd version v0.2.8

--- sd.rb
+++ sd.rb
@@ -1,9 +1,9 @@
 class Sd < Formula
   desc "A tool to keep utility scripts neatly organized."
   homepage "https://github.com/cv/sd"
-  url "https://github.com/cv/sd/releases/download/v0.2.7/sd_0.2.7_Darwin_x86_64.tar.gz"
-  version "0.2.7"
-  sha256 "bd485e7cc8758ab3798d1058df9effa6fc14b782e4749f9370711c70859830d4"
+  url "https://github.com/cv/sd/releases/download/v0.2.8/sd_0.2.8_Darwin_x86_64.tar.gz"
+  version "0.2.8"
+  sha256 "36c10cc55817ecd57c1c29e93883b9cd0f70c6d0be3f53b18c9d64c474cad38c"
 
   def install
     bin.install "sd"
